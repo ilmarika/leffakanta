6b277c9427430fb29cef50f30154def1020bba6d
Ilmari Kallioniemi <ilmari.linna@gmail.com>
2017-02-19 15:15:32 +0200

    Added GNUv2 copyright notices to the start of each file

--- editaction.php
+++ editaction.php
@@ -1,4 +1,20 @@
 <?php
+/*
+Copyright (C) 2017  Ilmari Kallioniemi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
 
 $user = 'admin';
 $pass = 'aad74af7a24d0c6e16a95657e9610954d5e7d909c83fc4b01abcfb674198dce9f928127d6688745e24f26c1b67fefd024e92af1fd5fa17b4feb478aa712b4384';
